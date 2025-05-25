cacde7d621acb95a125466469c886ba715b6b5ea
khushn <khushnood.naqvi@gmail.com>
2025-05-25 19:31:31 +0530

    Coded but not fully tested.

---
+++ traffic_lights/traffic_lights.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define int long long
+
+void debugprint(map<int, int> & mp);
+
+signed main() {
+
+	int x, n;
+	cin >> x >> n;
+
+	map<int, int> mp;
+	multiset<int> myset;
+	mp[x] = x;
+	//debugprint(mp);
+	myset.insert(x);
+	for (int i = 0; i<n; i++) {
+		int t;
+		cin >> t;
+		auto it = mp.lower_bound(t);		
+		
+		//cout << "trying to find in set, it->second: " << it->second << endl;
+		auto setit = myset.find(it->second);
+		if (setit != myset.end())
+			myset.erase(setit);
+
+		int next_light = it->first;
+		int part2 = next_light - t;
+		mp[next_light] = part2;
+		myset.insert(part2);
+
+		int part1 = t;
+		if (it != mp.begin()) {
+			it--;
+			part1 = t - it->first;
+		} 
+		mp[t] = part1;
+		myset.insert(part1);
+
+		//debugprint(mp);
+		cout << *myset.rbegin() << " ";
+	}
+	cout << endl;
+
+}
+
+void debugprint(map<int, int> & mp) {
+	cout << "map: " << endl;
+	for (auto it = mp.begin(); it != mp.end(); it++) {
+		cout << "key: " << it->first << ", value: " << it->second << endl;
+	}
+}
