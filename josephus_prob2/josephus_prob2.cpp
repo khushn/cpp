a7dc8340f277ff393ccd11a53baba57494d51b2b
khushn <khushnood.naqvi@gmail.com>
2025-05-28 18:51:51 +0530

    It worked by using an ordered set (part of GNU C++ PBDS)

--- josephus_prob2/josephus_prob2.cpp
+++ josephus_prob2/josephus_prob2.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
 #define int long long
+
+template<class T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 
 signed main() {
 	int n, k;
 	cin >> n >> k; 
 
-	vector<int> myvec(n);
+	ordered_multiset<int> ss;
+	//vector<int> myvec(n);
 	for (int i=0; i<n; i++) {
 		//myset.insert(i+1);
-		myvec[i] = i + 1;
+		//myvec[i] = i + 1;
+		ss.insert(i+1);
 	}
 
 
@@ -21,8 +29,11 @@ signed main() {
 	for (int i=0; i<n; i++) {
 		pos = (pos + k) % (n-i);
 		// cout << "debug pos: " << pos << endl;
-		cout << myvec[pos] << " ";
-		myvec.erase(myvec.begin() + pos);
+		auto it = ss.find_by_order(pos);
+		cout << *it << " ";
+		ss.erase(it);
+		//cout << myvec[pos] << " ";
+		//myvec.erase(myvec.begin() + pos);
 	}
 	cout << endl;
 
