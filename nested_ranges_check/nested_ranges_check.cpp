b8c55bed2c0f2f105cc0f134bb6e565c508d2444
khushn <khushnood.naqvi@gmail.com>
2025-06-03 18:53:52 +0530

    in the middle of using BIT in the problem

--- nested_ranges_check/nested_ranges_check.cpp
+++ nested_ranges_check/nested_ranges_check.cpp
@@ -11,23 +11,76 @@ using namespace std;
 // Node structure
 struct Node {
   int id;
-  int v;
-  bool start;
-  int v1; // other end (begin if v is end i.e. opposote) 
+  int x;
+  int y; // other end (begin if v is end i.e. opposote) 
 
-  Node(int id, int v, bool start, int v1) {
+  Node(int id, int x, bool start, int y) {
   	this->id = id;
-  	this->v = v;
-  	this->start = start;
-  	this->v1 = v1;
+  	this->x = x;
+  	this->y = y;
   }
 
   void print() {
-  	cout << "Node: id: " << this->id << ", v: " << this->v << ", start: " << this->start << ", v1: " << this->v1 << endl;
+  	cout << "Node: id: " << this->id << ", x: " << this->x << ", y: " << this->y << endl;
   }
 
 };
 
+/**
+Implementation of binary indexed tree (or Fenwick tree), 
+which is needed to solve this problem
+**/
+class BIT {
+
+public: 
+
+	vector<int> _bit;
+	int _n;
+
+	BIT(int n):_n(n), _bit(n) {
+	}
+
+	void update(int pos, int v) {
+		
+		pos +=1; // to make it 1 based index for the math to work
+		while ( pos <= _n) {
+			_bit[pos-1] += v;
+			pos += pos&-pos; // find the lowest bit set and increment by that
+		}
+	}
+
+	int query(int pos) {
+
+		int sum;
+
+		// to make it 1 based index for the math to work
+		pos +=1;
+		while (pos >= 1) {
+			sum += _bit[pos-1];
+			pos -= pos&-pos; // find the parent
+		}
+
+		return sum;
+	}
+
+	int query(int p1, int p2) {
+		int qp1 = query(p1);
+		int qp2 = query(p2);
+
+		cout << "query(" << p1 << ") = " << qp1 << endl;
+		cout << "query(" << p2 << ") = " << qp2 << endl;
+		
+		return qp2 - qp1;
+	}
+
+	void print() {
+		for (int i=0; i<_n; i++) {
+			cout << _bit[i] << " ";
+		}
+		cout << endl;
+	}
+
+};
 
 
 
@@ -38,45 +91,26 @@ signed main() {
 
 
 	vector<Node *> vec(n);
+
+	vector<int> all_y(n);
 	for (int i=0; i<n; i++) {
-		int a, b;
-		cin >> a >> b;
-		Node *n = new Node(i, a, true, b);
+		int x, y;
+		cin >> x >> y;
+		Node *n = new Node(i, x, true, y);
 		vec[i] = n;
+		all_y[i] = y;
 	}
 
+	// we need to have bigger range with same x on top. For that:
 
+	// first sord the y range in descending order
+	sort(vec.begin(), vec.end(), [](Node* lhs, Node* rhs) {
+	    return lhs->y > rhs->y;
+	});
 	
+	// then the x range in ascedding order
 	sort(vec.begin(), vec.end(), [](Node* lhs, Node* rhs) {
-	    return lhs->v < rhs->v;
-	});
-
-	/*
-	cout << " printing sorted list --- " << vec.size() << "----" << endl;
-	for (auto n: vec) {
-		n->print();
-	}
-
-	cout << "---------- " << endl;
-	*/
-
-	/*
-	sort(vec.begin(), vec.end(), [](Node* lhs, Node* rhs) {
-
-		
-	    if (lhs->v <= rhs->v && lhs->v1 >= rhs->v1)
-	        return true;
-	    // Ensure the opposite is false, so sort has consistent decisions
-	    if (rhs->v <= lhs->v && rhs->v1 >= lhs->v1)
-	        return false;
-	     
-	    // Fallback: sort by a ascending
-	    return (lhs->v1 - lhs->v < rhs->v1 - rhs->v);
-	});
-	*/
-
-	sort(vec.begin(), vec.end(), [](Node* lhs, Node* rhs) {
-	    return lhs->v1 > rhs->v1;
+	    return lhs->x < rhs->x;
 	});
 
 	
@@ -87,10 +121,45 @@ signed main() {
 
 	cout << "---------- " << endl;
 	
+	cout << "compressed ys" << endl;
+	sort(all_y.begin(), all_y.end());
+	map<int, int> compy;
+	
+	int prev = all_y[0];
+	int idx=0;
+	compy[all_y[0]] = idx++;
+	for (int i=1; i<n; i++) {
+		int cur = all_y[i];
+		if (prev != cur)
+			compy[cur] = idx++;
 
+		prev = cur;
+	}
+
+	for (auto it = compy.begin(); it != compy.end() ; it++ ) {
+		cout << it->first << " => " << it->second << endl;
+	}
+	cout << "---" << endl;
 
 	vector<int> has_others(n);
 	vector<int> is_inside(n);
+
+	int compy_sz = compy.size();
+	BIT bit(compy_sz);
+	for (Node * nd: vec) {
+		int y = compy[nd->y];
+		int qval = bit.query(y, compy_sz);
+		cout << "query(" << y << ", " << compy_sz << ") = " << qval << endl;
+		if (qval >= 1)
+			is_inside[nd->id];
+
+		bit.update(y, 1);
+		bit.print();
+	}
+
+
+
+	
 
 	
 
