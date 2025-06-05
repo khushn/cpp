7cb0a8f7ac78578b6164050206954661f43b9822
khushn <khushnood.naqvi@gmail.com>
2025-06-05 19:09:56 +0530

    This got accepted. Yay! I had to learn binary indexed tree (BIT), also called Fenwick tree for the same.

--- nested_ranges_check/nested_ranges_check.cpp
+++ nested_ranges_check/nested_ranges_check.cpp
@@ -38,6 +38,12 @@ public:
 	int _n;
 
 	BIT(int n):_n(n), _bit(n) {
+		/*
+		cout << "BIT constructor, _bit vector: " << endl;
+		for (int i=0; i<n; i++) {
+			cout << "_bit[" <<  i << "] = " << _bit[i] << endl;;
+		}
+		*/
 	}
 
 	void update(int pos, int v) {
@@ -51,7 +57,7 @@ public:
 
 	int query(int pos) {
 
-		int sum;
+		int sum = 0;
 
 		// to make it 1 based index for the math to work
 		pos +=1;
@@ -64,11 +70,16 @@ public:
 	}
 
 	int query(int p1, int p2) {
-		int qp1 = query(p1);
+
+		int qp1 = 0;
+		p1--;
+		if (p1 >= 0) {
+			qp1 = query(p1);
+		}
 		int qp2 = query(p2);
 
-		cout << "query(" << p1 << ") = " << qp1 << endl;
-		cout << "query(" << p2 << ") = " << qp2 << endl;
+		// cout << "query(" << p1 << ") = " << qp1 << endl;
+		// cout << "query(" << p2 << ") = " << qp2 << endl;
 		
 		return qp2 - qp1;
 	}
@@ -110,18 +121,24 @@ signed main() {
 	
 	// then the x range in ascedding order
 	sort(vec.begin(), vec.end(), [](Node* lhs, Node* rhs) {
+
+		// this is important to check again, as it may do it randomly for equal x (otherwise)
+		if (lhs->x == rhs->x)
+			return lhs->y > rhs->y;
+
 	    return lhs->x < rhs->x;
 	});
 
-	
+	/*
 	cout << " printing sorted list --- " << vec.size() << "----" << endl;
 	for (auto n: vec) {
 		n->print();
 	}
 
 	cout << "---------- " << endl;
+	*/
 	
-	cout << "compressed ys" << endl;
+	// cout << "compressed ys" << endl;
 	sort(all_y.begin(), all_y.end());
 	map<int, int> compy;
 	
@@ -136,32 +153,49 @@ signed main() {
 		prev = cur;
 	}
 
+	/*
 	for (auto it = compy.begin(); it != compy.end() ; it++ ) {
 		cout << it->first << " => " << it->second << endl;
 	}
 	cout << "---" << endl;
+	*/
 
+
+	// cout << "--- filling has_others --- " << endl;
 	vector<int> has_others(n);
-	vector<int> is_inside(n);
+	BIT bit1(compy.size());
+	int compy_max = compy.size() - 1;	
+	for (int i=n-1; i>=0; i--) {
+		Node *nd = vec[i];
+		int y = compy[nd->y];
+		int q = bit1.query(y);
+		// cout << "query(" << y << ") = " << q << endl;
+		if (q >= 1) {
+			has_others[nd->id] = 1;
+		}
+		bit1.update(y, 1);
+		// bit1.print();
+	}
+	// cout << "------- " << endl;
 
-	int compy_sz = compy.size();
-	BIT bit(compy_sz);
+
+	// cout << "--- filling is_inside --- " << endl;
+	vector<int> is_inside(n);
+	BIT bit(compy.size());
+	compy_max = compy.size() - 1;	
 	for (Node * nd: vec) {
 		int y = compy[nd->y];
-		int qval = bit.query(y, compy_sz);
-		cout << "query(" << y << ", " << compy_sz << ") = " << qval << endl;
-		if (qval >= 1)
-			is_inside[nd->id];
-
+		int qval = bit.query(y, compy_max);
+		// cout << "query(" << y << ", " << compy_max << ") = " << qval << endl;
+		if (qval >= 1) {
+			// cout << "setting is_inside for nd->id: " << nd->id << endl;
+			is_inside[nd->id] = 1;
+		}
 		bit.update(y, 1);
-		bit.print();
+		// bit.print();
 	}
+	// cout << "------- " << endl;
 
-
-
-	
-
-	
 
 	for (int i=0; i<n; i++) {
 		cout << has_others[i] << " ";
