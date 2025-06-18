cf634d08d0ed80dd0ac6d186699d71aee41326b4
khushn <khushnood.naqvi@gmail.com>
2025-06-18 18:31:26 +0530

    Learnt the hard way, that I needed to solve it using binary search in the answer space 1 to t * min_k. It was easy to code. Passed in 1st try!!

--- factory_machines/factory_machines.cpp
+++ factory_machines/factory_machines.cpp
@@ -15,56 +15,39 @@ signed main() {
 	int n, t;
 	cin >> n >> t;
 
-	map<vector<int>, int> mymap;
-	//vector<int> vec(n);
+	vector<int> vec(n);
+	int k_min = INT_MAX;
 	for (int i=0; i<n; i++) {
 		int k;
 		cin >> k;
-		vector<int> v(3);
-		v[0] = k; // sorting order k + h
-		v[1] = k;
-		v[2] = 0;
-		if (mymap.find(v) != mymap.end() ) {
-			int count = mymap[v];
-			mymap[v] = count+1;
-
-		} else
-			mymap[v] = 1;
+		vec[i] = k;
+		if (k < k_min)
+			k_min = k;
 	}
 
-	
-	// debug print
-	
-	cout << "debug print " << endl;
-	for(auto it = mymap.begin(); it != mymap.end(); it++) {
-		vector<int> v = it->first;
-		for (int i=0; i<3; i++)
-			cout << v[i] << " ";
-		cout << " -> " << it->second << endl;
-	}
-	cout << "-------" << endl;
 
+	int l1 = 1;
+	int l2 = k_min * t;
 
+	while (l1 < l2) {
+		
+		int mid = (l1 + l2) /2;
+		//cout << "l1: " << l1 << ", l2: " << l2 << ", mid: " << mid << endl ;
 
-	int minh = INT_MAX;
-	float div = 0.0;
-	for(auto it = mymap.begin(); it != mymap.end(); it++) {
-		vector<int> v = it->first;
-		float h = v[1];
-		if (h >= minh) {
-			cout << "ignoring h: " << h << " as more than minh: " << minh << endl;
-			continue;
+		int tot_p = 0;
+		for (int i=0; i<n; i++) {
+			tot_p += mid / vec[i];
 		}
-		float count = it->second;
-		div += count / h;
-		cout << "div: " << div << endl;
-		minh = t / div;
-		cout << "minh: " << minh << endl;
-	}
 
+		//cout << "tot_p: " << tot_p << endl;
+		if (tot_p < t)
+			l1 = mid+1;
+		else 
+			l2 = mid;
+	}
 	
 
-	cout << minh << endl;
+	cout << l2 << endl;
 
 }
 
---
+++ factory_machines/factory_machines_3.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+#define int long long
+
+void print_vec(vector<int> v);
+
+signed main() {
+
+	int n, t;
+	cin >> n >> t;
+
+	map<vector<int>, int> mymap;
+	//vector<int> vec(n);
+	for (int i=0; i<n; i++) {
+		int k;
+		cin >> k;
+		vector<int> v(3);
+		v[0] = k; // sorting order k + h
+		v[1] = k;
+		v[2] = 0;
+		if (mymap.find(v) != mymap.end() ) {
+			int count = mymap[v];
+			mymap[v] = count+1;
+
+		} else
+			mymap[v] = 1;
+	}
+
+	
+	// debug print
+	
+	cout << "debug print " << endl;
+	for(auto it = mymap.begin(); it != mymap.end(); it++) {
+		vector<int> v = it->first;
+		for (int i=0; i<3; i++)
+			cout << v[i] << " ";
+		cout << " -> " << it->second << endl;
+	}
+	cout << "-------" << endl;
+
+
+
+	int minh = INT_MAX;
+	float div = 0.0;
+	for(auto it = mymap.begin(); it != mymap.end(); it++) {
+		vector<int> v = it->first;
+		float h = v[1];
+		if (h >= minh) {
+			cout << "ignoring h: " << h << " as more than minh: " << minh << endl;
+			continue;
+		}
+		float count = it->second;
+		div += count / (float) h;
+		cout << "div: " << div << endl;
+		minh = t / div;
+		cout << "minh: " << minh << endl;
+	}
+
+	
+
+	cout << minh << endl;
+
+}
+
+
+void print_vec(vector<int> v) {
+	for (int i=0; i<v.size(); i++)
+		cout << "v[" << i << "] = " << v[i] << ", ";
+	cout << endl;
+}
