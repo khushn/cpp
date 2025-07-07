350826ca4068b4835a242fa29810060c739d810f
khushn <khushnood.naqvi@gmail.com>
2025-07-07 18:59:58 +0530

    Got wrong answer as forgot to modulo !!

---
+++ dp/coin_combinations_1/coin_combinations_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+	int n, x;
+	cin >> n >> x;
+
+	vector<int> v(n);
+	for(int i=0; i<n; i++) {
+		cin >> v[i];
+	}
+
+	vector<int> m(x+1);
+	m[0] = 1;
+	for(int i=1; i<=x; i++) {
+		
+		for (int j=0; j<n; j++) {
+			int c = v[j];
+			if (i-c < 0)
+				continue;
+			
+			m[i] += m[i-c];
+
+			
+
+		}
+	}
+
+	
+	cout << m[x] << endl;
+}
