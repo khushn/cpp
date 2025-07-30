971cba77c4f6aeb7123010bd982669708bfc3998
khushn <khushnood.naqvi@gmail.com>
2025-07-30 14:54:20 +0530

    Avoding using a set, just using a list (and maintaining a visited nodes 2d array). It worked!

--- graph/counting_rooms/counting_rooms.cpp
+++ graph/counting_rooms/counting_rooms.cpp
@@ -5,34 +5,47 @@
 
 using namespace std;
 
+/*
+Just counting number of connected graphs, which would translate to counting rooms.
+2nd attempt to avoid TLE (not using a set in this case)
+*/
+
 signed main() {
 	int n, m;
 	cin >> n >> m;
 
 	vector<vector<int>> v(n, vector<int>(m));
-	set<pair<int, int>> nodes;
+	vector<vector<bool>> visited(n, vector<bool>(m));
+	//set<pair<int, int>> nodes;
+	vector<pair<int, int>> nodes;
 	for(int i=0;i<n; i++ ) {
 		for(int j=0; j<m; j++) {
 			char ch;
 			cin >> ch;
 			v[i][j] == ch;
-			if (ch == '.')
-				nodes.insert(make_pair(i, j));
+			if (ch == '.') {				
+				nodes.push_back(make_pair(i, j));
+			} else {
+				visited[i][j] = true; // need it for wall, for traversal later on
+			}
 
 		}
 	}
 
-	//cout << "nodes.size() : " << nodes.size() << endl;
+	// cout << "nodes.size() : " << nodes.size() << endl;
 
 	int num_connected_graphs = 0;
-	while(!nodes.empty()) {
-		auto it = nodes.begin();
-		int i = it->first;
-		int j = it->second;
-		//cout << "coming in for graph, having node i: " << i << ", j: " << j << endl;
+	for(auto &node : nodes) {
+		int i = node.first;
+		int j = node.second;
+		if (visited[i][j])
+			continue;
+
+		// cout << "coming in for graph, having node i: " << i << ", j: " << j << endl;
 		queue<pair<int, int>> q;
 		q.push(make_pair(i, j));
-		nodes.erase(make_pair(i, j));
+		visited[i][j] = true;
+		//nodes.erase(make_pair(i, j));
 		while(!q.empty()) {
 			auto elem = q.front();
 			q.pop();
@@ -41,27 +54,31 @@ signed main() {
 			
 
 			// check top
-			if (i>0 && nodes.find(make_pair(i-1, j)) != nodes.end()) {
+			if (i>0 && !visited[i-1][j]) {
 				q.push(make_pair(i-1, j));
-				nodes.erase(make_pair(i-1, j));
+				//nodes.erase(make_pair(i-1, j));
+				visited[i-1][j] = true;
 			}
 
 			// check right
-			if (j<m-1 && nodes.find(make_pair(i, j+1)) != nodes.end()) {
+			if (j<m-1 && !visited[i][j+1]) {
 				q.push(make_pair(i, j+1));
-				nodes.erase(make_pair(i, j+1));
+				//nodes.erase(make_pair(i, j+1));
+				visited[i][j+1] = true;
 			}
 
 			// check left
-			if (j>0 && nodes.find(make_pair(i, j-1)) != nodes.end()) {
+			if (j>0 && !visited[i][j-1]) {
 				q.push(make_pair(i, j-1));
-				nodes.erase(make_pair(i, j-1));
+				//nodes.erase(make_pair(i, j-1));
+				visited[i][j-1] = true;
 			}
 
 			// check bottom
-			if (i<n-1 && nodes.find(make_pair(i+1, j)) != nodes.end()) {
+			if (i<n-1 && !visited[i+1][j]) {
 				q.push(make_pair(i+1, j));
-				nodes.erase(make_pair(i+1, j));
+				//nodes.erase(make_pair(i+1, j));
+				visited[i+1][j] = true;
 			}
 		}
 		
