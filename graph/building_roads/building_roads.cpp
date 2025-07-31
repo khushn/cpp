7488c3414a04ba69de8a9c12717f7c4a2512cf9d
khushn <khushnood.naqvi@gmail.com>
2025-07-31 17:06:59 +0530

    Works. Uses a set. So O(N(log(N) + M)

---
+++ graph/building_roads/building_roads.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+/*
+Algo:
+1. Just populated the graph in vec of vec structure (outer is all the n nodes)
+2. Put each connected graph unique node in another vec (using a set for bookkeeping)
+3. just form k-1 edhes in the k uniqie nodes (one per each connected graph)
+*/
+
+signed main() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> graph(n+1, vector<int>());
+	for(int i=0; i<m; i++) {
+		int n1, n2;
+		cin >> n1 >> n2;
+		graph[n1].push_back(n2);
+		graph[n2].push_back(n1);
+	}
+
+	/*
+	// debug print
+	for(int i=1; i<=n; i++) {
+		vector<int> nodes = graph[i];
+		cout << i << " -- ";
+		for(int nd: nodes)
+			cout << nd << " ";
+		cout << endl;
+	}
+	cout << "----" << endl;
+	*/
+
+	vector<int> unique_graph_nodes;
+	set<int> nodes_used;
+	for(int i=1; i<=n; i++) {
+
+		if (nodes_used.find(i) != nodes_used.end())
+			continue;
+
+		unique_graph_nodes.push_back(i);
+
+		queue<int> q;
+		q.push(i);
+		while(!q.empty()) {
+			int nd = q.front();
+			q.pop();
+
+			if (nodes_used.find(nd) != nodes_used.end())
+				continue;
+			
+			nodes_used.insert(nd);
+
+			vector<int> child_nodes = graph[nd];
+			for (int ch: child_nodes)
+				q.push(ch);
+				
+		}		
+	}
+
+	cout << unique_graph_nodes.size() - 1 << endl;
+
+	for(int i=0; i<unique_graph_nodes.size()-1; i++)
+		cout << unique_graph_nodes[i] << " " << unique_graph_nodes[i+1] << endl;
+
+}
