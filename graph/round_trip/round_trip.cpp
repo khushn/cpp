cf6c5fd413b12989aa659872619d118df904b547
khushn <khushnood.naqvi@gmail.com>
2025-08-05 17:47:24 +0530

    Functionally correct. But gives core dump for big test cases. recursion causes stack overflow. Need to avoid using recursion, but still do a DFS.

---
+++ graph/round_trip/round_trip.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+/*
+Simple cycle detection algo. 
+Need to use DFS for this (although I am prefering BFS for all graph problems so far i CSES)
+Maintain a visited list, to process fast
+*/
+
+const int N = 10e5+1;
+vector<int> graph[N];
+vector<bool> visited(N);
+map<int, int> g_portion;
+
+
+bool find_cycle(int nd1) {
+
+
+	for(int nd2: graph[nd1]) {
+
+		auto it = g_portion.find(nd2);
+		if (it != g_portion.end() && it->second == nd1)
+			continue;
+
+		g_portion[nd1] = nd2;
+		
+		cout << nd1 << "->" << nd2 << endl;
+		
+		if (g_portion.find(nd2) != g_portion.end()) {		
+			// found cycle !!
+			
+			/*
+			// debug
+			for (const auto & it: g_portion) {
+				cout << it.first << "->" << it.second << endl;
+			}
+			*/
+		
+			vector<int> print_vec;
+			print_vec.push_back(nd2);
+			int cur_node = g_portion[nd2];
+			while (cur_node != nd2 && cur_node != 0) {
+				print_vec.push_back(cur_node);
+				cur_node = g_portion[cur_node];
+			}
+			print_vec.push_back(nd2);
+
+			cout << print_vec.size() << endl;
+			for(int k: print_vec)
+				cout << k << " ";
+			cout << endl;
+			return true;			
+		}
+
+		
+		if (find_cycle(nd2))
+			return true;
+
+		g_portion.erase(nd1);
+		cout << "------ erasing for " << nd1 << endl;
+	}
+	return false;
+}
+
+signed main() {
+
+	int n, m;
+	cin >> n >> m;
+
+	for(int i=0; i<m; i++) {
+		int l, r;
+		cin >> l >> r;
+		graph[l].push_back(r);
+		graph[r].push_back(l);
+	}
+
+	for(int i=1; i<=n; i++) {
+
+		cout << "coming in for node " << i << endl;
+
+		bool found = find_cycle(i);
+		if (found)
+			return 0;
+
+		//g_portion.clear();
+
+		cout << "=====" << endl;
+
+	}
+	
+	cout << "IMPOSSIBLE" << endl;
+}
