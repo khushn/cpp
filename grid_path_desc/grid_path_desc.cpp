45d0c6515491f680764288ce2f8425a8ee3bbf99
khushn <khushnood.naqvi@gmail.com>
2025-08-07 19:24:38 +0530

    Tried today. Ruled out some approaches. Also added a new intermediate file, with some more data

--- grid_path_desc/grid_path_desc.cpp
+++ grid_path_desc/grid_path_desc.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <tuple>
+#include <map>
 
 #define ulong unsigned long long
 
@@ -36,10 +37,11 @@ string binary_print(ulong v) {
 }
 
 // all four directions
-vector<pair<int, int>> dirs = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
+// the extra two coordinates are for the 2nd cell in the same direction
+vector<vector<int>> dirs = {{0,1, 0, 2}, {0, -1, 0, -2}, {1, 0, 2, 0}, {-1, 0, -2, 0}};
 
 // not to cover all end cells
-
+/*
 vector<vector<pair<int, int>>> check_cells = { {{6, 0} },
 									{ {5, 0}, {6, 1} },
 									{ {4, 0}, {5, 1}, {6, 2} },
@@ -47,6 +49,12 @@ vector<vector<pair<int, int>>> check_cells = { {{6, 0} },
 									{ {2, 0}, {3, 1}, {4, 2}, {5, 3}, {6, 4} }, 
 									{ {1, 0}, {2, 1}, {3, 2}, {4, 3}, {5, 4}, {6, 5} }, 
 									{ {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6} } };
+*/
+
+// keep till 3 prev steps only
+vector<vector<pair<int, int>>> check_cells = { {{6, 0} },
+									{ {5, 0}, {6, 1} },
+									{ {4, 0}, {5, 1}, {6, 2} } };									
 									
 
 signed main() {
@@ -99,14 +107,71 @@ signed main() {
 			
 			// cout << "x: " << x << ", y: " << y << endl;
 			// cout << "route: " << binary_print(route) << endl;
-			
+
+
+			/*
+			// Approach 3
+			bool must_follow_this_dir = false;
+			// check if its 2nd cell in the same direction is filled, 
+			// in that case we need to take it (TBD: Explain on this logic if it works!)
+			for(auto dir : dirs) {
+				int x1 = x + dir[0];
+				int y1 = y + dir[1];
+				if (x1 < 0 || y1 <  0 || x1 > GRID_SIZE - 1 || y1 > GRID_SIZE - 1)
+					continue;
+
+				int seq = co_ord_to_seq(x1, y1);				
+				if (x1 == 0 && x == 1 && y == y1 && !is_bit_set (route, seq-1)) {				
+					int x2 = x + dir[2];
+					int y2 = y + dir[3];
+
+					ulong new_route = route|(1ull<<(seq-1));
+
+					
+					
+					//if (x2 < 0 || y2 <  0 || x2 > GRID_SIZE - 1 || y2 > GRID_SIZE - 1)
+					//	must_follow_this_dir = true;
+					
+					//else {
+					//	int seq2 = co_ord_to_seq(x2, y2);
+					//	if (is_bit_set (route, seq2-1)) {
+					//		must_follow_this_dir = true;
+					//	}
+					//}
+
+					must_follow_this_dir = true;
+
+					if (must_follow_this_dir) {
+						new_cur_pos_list.push_back(make_pair(seq, new_route));
+						// we don't even want to consider other directions in this case
+						break;
+					}
+					
+				}
+
+			}
+
+			if (must_follow_this_dir)
+				continue;
+				
+			*/
+
 
 			
 			for(auto dir : dirs) {
-				int x1 = x + dir.first;
-				int y1 = y + dir.second;
+				int x1 = x + dir[0];
+				int y1 = y + dir[1];
 				if (x1 < 0 || y1 <  0 || x1 > GRID_SIZE - 1 || y1 > GRID_SIZE - 1)
 					continue;
+
+				/*
+				// ignore the case when reach destination cell before the last step
+				if (s < NUM_STEPS) {
+					if (x1 == GRID_SIZE - 1 && y1 == 0)
+						continue;
+				}
+				*/
+
 
 				// approach 2 -- need to make it work. Its fast!!
 				bool prev_filled = true;
@@ -120,7 +185,7 @@ signed main() {
 							break;
 						}
 					}
-				}
+				} 
 
 				if (!prev_filled)
 					continue;
@@ -155,6 +220,35 @@ signed main() {
 							break;
 						}
 					}
+
+				}
+
+				if (!prev_filled)
+					continue;
+
+				// NOW check for the inner sides of 5x5 grid, they should move in the opposite direction
+				prev_filled = true;
+				if (x1 == 1) {
+					// if top row is filled
+					int num_filled = 0;
+					for(int yy = 0; yy< GRID_SIZE-1; yy++) {
+						int s1 = co_ord_to_seq(0, yy);
+						if (is_bit_set(route, s1 - 1)) {
+							num_filled++;
+						}
+					}
+
+					if (num_filled == GRID_SIZE) {
+						// now check the rhs filled of the current row
+						for(int yy=y1+1; yy<GRID_SIZE; yy++){
+							int s1 = co_ord_to_seq(x1, yy);
+							if (!is_bit_set(route, s1 - 1)) {
+								prev_filled = false;
+								break;
+							}
+						}
+
+					}
 				}
 
 				if (!prev_filled)
@@ -167,7 +261,7 @@ signed main() {
 
 
 					/*
-					// Approach 1 -- didnot work
+					// Approach 1 -- keep till 3 prev steps only
 					int num_prev_steps = check_cells.size();
 
 					for(int k=0; k<num_prev_steps; k++) {
@@ -189,6 +283,8 @@ signed main() {
 						}
 					}
 					*/
+					
+
 						
 					if (can_add)
 						new_cur_pos_list.push_back(make_pair(seq, new_route));
@@ -209,6 +305,17 @@ signed main() {
 		}
 		*/
 		
+		if (s >= NUM_STEPS - 4) {
+			map<pair<int, int>, int> count_map;
+			for(auto p: new_cur_pos_list) {
+				int x, y;
+				tie(x, y) = seq_to_co_ord(p.first);
+				count_map[make_pair(x, y)] += 1;
+			}
+			for (auto & it: count_map) {
+				cout << "x: " << it.first.first << ", y: " << it.first.second << ", count: " << it.second << endl;
+			}
+		}
 		
 		
 		
