b58b0615a66bf63bb5285e709245698a91a3ff67
khushn <khushnood.naqvi@gmail.com>
2025-09-02 19:30:40 +0530

    It worked!! See code comment for final algo. (needed to take care of self edge as a special case based on test case 25)

--- graph/high_score/high_score.cpp
+++ graph/high_score/high_score.cpp
@@ -8,6 +8,21 @@ using namespace std;
 
 #define int long long
 
+/*
+Algo: 
+This one was bit tough. Had to make several tries (after getting the basic idea)
+Final approach put below:
+0. Initialize all incoming edges (paths) to -ve infinity for all nodes
+1. Use a priority queue (instead of just queue) based on weights (incoming to a node). 
+    We don't have to do anything special. Just keep weight second in the tuple, and it works on its own
+    as node is the same (test case 25 showed this need)
+2. Ignore incoming edges if weight is less than existing
+3. Add some cases to break early (not sure if its essential)
+4. In the end print high_scores[n]
+
+
+*/
+
 signed main() {
 	int n, m;
 	cin >> n >> m;
@@ -21,23 +36,22 @@ signed main() {
 
 
 	vector<int> max_scores(n+1, LONG_MIN);
+	// queue<tuple<int, int, int>> q;
 	priority_queue<tuple<int, int, int>> q;
 	q.push(make_tuple(1, 0, 0));
 	while(!q.empty()) {
+		// auto t = q.front(); q.pop();
 		auto t = q.top(); q.pop();
-		int n1 = get<0>(t);
-		int plen = get<1>(t);
-		int w = get<2>(t);
-		//cout << "n1: " << n1 << ", plen: " << plen << ", w: " << w << endl;
+		int n1 = get<0>(t);		
+		int w = get<1>(t);
+		int plen = get<2>(t);
+		// cout << "incoming node, n1: " << n1 << ", plen: " << plen << ", w: " << w << endl;
 		/*
 		if (plen > n*3) {
 			// abandon this path 
 			continue;
 		}
 		*/
-
-		if (n1 == n && w == LONG_MAX)
-			break;
 
 		
 		if (w > max_scores[n1]) {
@@ -50,7 +64,9 @@ signed main() {
 		} else {
 			continue;
 		}
-			
+		
+		if (n1 == n && w == LONG_MAX)
+			break;
 		
 
 		for(auto p: graph[n1]) {
@@ -61,7 +77,12 @@ signed main() {
 				nw = w;
 			else
 				nw = w + w2;
-			q.push(make_tuple(n2, plen+1, nw));			
+
+			// case of 1 1 2 (edge to self having positive weight as in test case no. 25)
+			if (n1 == n2 && w2 > 0)
+				nw = LONG_MAX;
+
+			q.push(make_tuple(n2, nw, plen+1));			
 		}
 
 	}
