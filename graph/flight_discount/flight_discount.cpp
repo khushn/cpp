d3bd557d8ce48744da95095a9b61d9b5afa96ae5
khushn <khushnood.naqvi@gmail.com>
2025-09-03 18:34:52 +0530

    Gives TLE in 5 cases, rest work fine. Need to think...

--- graph/flight_discount/flight_discount.cpp
+++ graph/flight_discount/flight_discount.cpp
@@ -16,57 +16,67 @@ signed main() {
 	for(int i=0; i<m; i++) {
 		int a, b, w;
 		cin >> a >> b >> w;
-		graph[a].push_back(make_pair(b, w));
+		graph[a].push_back(make_pair(b, -w));
 	}
 
 
 	// dp array of min cost to any city
 	// sum and max(x)
-	vector<pair<int, int>> city_cost(n+1, {LONG_MAX, LONG_MAX}); 
-	priority_queue<tuple<int, int, int>> q;
+	vector<pair<int, int>> city_cost(n+1, {LONG_MIN, LONG_MIN}); 
+	priority_queue<vector<int>> q;
+	//queue<tuple<int, int, int>> q;
 	// priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> q;
 
-	q.push(make_tuple(1, 0, 0));
+	q.push({1, 0, 0, 0});
 
 	while(!q.empty()) {
 		auto t = q.top(); q.pop();
-		int n1 = get<0>(t);		
-		int c = get<1>(t);
-		int x = get<2>(t);
+		// auto t = q.front(); q.pop();
+		int n1 = t[0];		
+		int c = t[1];
+		int x = t[2];
+		int nn = t[3];
+
+		if (nn >= n)
+			continue;
 
 		//cout << "incoming n1: " << n1 << ", c: " << c << ", x: " << x << endl;
 		// check if incoming city cost is greater than existing 
 		// then ignore
-		int ac = c - x + x/2;
+		//int ac = c - x + x/2;
 		auto p2 = city_cost[n1];
 		int c2 = p2.first;
 		int x2 = p2.second;
-		int ec = c2 - x2 + x2/2;
-		if (c > c2) {
+		//int ec = c2 - x2 + x2/2;
+		
+		if (c <= c2 && x <= x2) {
 			continue;
+		} else {
+			if (c > c2)
+				c2 = c;
+			if (x > x2)
+				x2 = x;
+			cout << "assigning n1: " << n1 << ", c: " << c2 << ", x: " << x2 << endl;
+			city_cost[n1] = make_pair(c2, x2);
+
 		}
+		
 
-		//cout << "assigning n1: " << n1 << ", c: " << c << ", x: " << x << endl;
-		city_cost[n1] = make_pair(c, x);
-
+		
 		// now we know cost is less
 		// we can use it
 		for( auto p: graph[n1]) {
 			int n2 = p.first;
-			int c2 = p.second;
-			int tc = c + c2;
-			int x2 = x;
-			if (c2 > x2)
-				x2 = c2;
-			q.push(make_tuple(n2, tc, x2));
+			int w2 = p.second;
+			int ndc = c2 + w2;
+			int dc = c2 + w2/2;
+			if (x2 + w2 > dc)
+				dc = x2 + w2;
+			q.push({n2, ndc, dc, nn+1});
 
 		}
 	}
 
-
-	int tc = city_cost[n].first;
-	int x = city_cost[n].second;
-	tc = tc - x + x/2;
-	cout << tc << endl;
+	cout << -city_cost[n].second << endl;
 
 }
