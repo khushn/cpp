ed57f739617c65c8b1d786010f3ecaa7df68414b
khushn <khushnood.naqvi@gmail.com>
2025-09-30 17:46:16 +0530

    Done. Gives core dump locally. But got accepted! Probably due to higher call stack size. Reaidng problem analysis now.

--- tree/tree_matching/tree_matching.cpp
+++ tree/tree_matching/tree_matching.cpp
@@ -1,32 +1,61 @@
 #include <iostream>
 #include <set>
+#include <vector>
 
 using namespace std;
 
 
 /*
-Trying a very simple algo using a hash set. 
+Approach 2. 
+1. construct the tree
+2. Randomly assign a node as root. 
+3. Traverse using dfs
+4. Maintain used edges in a hash set. 
+5. Add to hashset only if both nodes not present and count it.
 Just based on what needs to be done (not even considering tree pov)
 
 */
 
+int uniq_pairs = 0;
+set<int> myset;
+vector<vector<int>> adj;
+
+void dfs(int s, int e);
+
 signed main() {
 	int n;
 	cin >> n;
-	set<int> myset;
-	int uniq_pairs = 0;
+	adj.resize(n+1);
+	
+	
 	for(int i=0; i<n-1; i++) {
 		int a, b;
 		cin >> a >> b;
-		if (myset.find(a) == myset.end() && myset.find(b) == myset.end()) {
-			uniq_pairs++;
-			myset.insert(a);
-			myset.insert(b);
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+
+	dfs(1,0);
+
+	cout << uniq_pairs << endl;
+}
+
+void dfs(int s, int e) {
+
+	for(auto n: adj[s]) {
+		if (n != e) {
+			//cout << "recursive call dfs(" << n << ", " << s << ")" << endl;
+			dfs(n, s);
 		}
 	}
 
-	if (n-uniq_pairs -1 > uniq_pairs)
-		uniq_pairs = n-uniq_pairs -1;
-	
-	cout << uniq_pairs << endl;
+	if (e!= 0) {
+		
+		if (myset.find(s) == myset.end() && myset.find(e) == myset.end()) {
+			uniq_pairs++;
+			//cout << "uniq_pairs: " << uniq_pairs << endl;
+			myset.insert(s);
+			myset.insert(e);
+		}
+	}
 }
